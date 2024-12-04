369c653d2006eb3e741a52d735d7002b23a57eaa
DEVESH KUMAR SINGH <deveshkmr318@gmail.com>
2024-12-04 22:01:40 +0530

    bubbleSort

---
+++ BubbleSorting.java
@@ -0,0 +1,44 @@
+
+public class BubbleSorting{
+
+    private static int[] table = null;
+
+    private static void bubbleSort(){
+
+        for (int i = 0; i < table.length - 1; i++) {
+            for (int j = i + 1; j < table.length; j++) {
+                
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        table = new int[]{4,2,6,1,5,3};
+
+        printArray();
+
+        bubbleSort();
+
+        printArray();
+
+
+    }
+
+    private static void printArray(){
+
+        System.out.println("  ");
+
+        System.out.print("  [ ");
+
+        for(int i = 0 ; i < table.length ; i++){
+            System.out.print( table[i] + " ");
+        }
+
+        System.out.print("]  ");
+
+        System.out.println("   ");
+    }
+
+}
