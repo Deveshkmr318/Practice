c5cc121e9da31edb31d64190a3f8b7808d6e6b36
DEVESH KUMAR SINGH <deveshkmr318@gmail.com>
2025-01-20 13:20:31 +0530

    CHANGES

--- StringMatching/RabinKarp.java
+++ StringMatching/RabinKarp.java
@@ -2,19 +2,35 @@ package StringMatching;
 
 public class RabinKarp {
 
+    static final int MUL = 256;
+    static final int MOD = 101;
+    static int h = 1;
+
     public static void main(String[] args) {
-        
-        String text = "";
+                    //           111111111122222222223333333333444
+                    // 0123456789012345678901234567890123456789012
+        String text = "DEVESH KUMAR SINGH MARCH MEIN MARTA HAI MAR";
 
-        String pat = "";
+        String pat = "MAR";
 
-        int x = patMatch(text, pat);
+        System.out.println("======RK======");
 
-        System.out.println("");
+        patMatch(text, pat);
+
+        // System.out.println("");
 
     }
+
+    private static void preCompute(int m){
+
+
+        for(int i = 0 ; i < m-1 ; i++){
+            h = (h*MUL)%MOD;
+        }
+
+    }   
         
-    private static int patMatch(String text, String pat) {
+    private static void patMatch(String text, String pat) {
 
         int m = text.length();
         int n = pat.length();
@@ -22,12 +38,58 @@ public class RabinKarp {
         int textHash = 0;
         int patHash = 0;
 
+        preCompute(n);
+
         for(int i = 0 ; i < n ; i++){
-            textHash += text.charAt(i);
-            patHash += pat.charAt(i);
+
+            textHash = (textHash*MUL + text.charAt(i))%MOD;
+            patHash = (patHash*MUL + pat.charAt(i))%MOD;
+
         }
+
+        int i = n;
+
+        for(i = n ; i <= m ; i++){
+
+            if(textHash == patHash){
+
+                if(isEqual(text, pat, i-n)){
+                
+                    System.out.println("Pattern is Found at " + (i-n));
+                
+                }
+            }
+
+            if(i < m){
+
+                textHash = textHash - h*text.charAt(i-n);
+                textHash = (textHash*MUL + text.charAt(i))%MOD;
+
+                if(textHash < 0){
+                    textHash = textHash + MOD;
+                }
+
+            }
+                
+        }
+        
+    }
+                
+    private static boolean isEqual(String text, String pat, int start) {
+
+        int l = pat.length();
+
+        boolean isSame = true;
+        
+        for(int i = start; i < l; i++){
             
-        return -1;
+            if(text.charAt(i) != pat.charAt(i-start)){
+                isSame = false;
+                break;
+            }
+        }
+        
+        return isSame;
     }
     
 }
